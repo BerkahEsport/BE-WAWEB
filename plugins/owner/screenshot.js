5c78d8f48562ab6240291ddaf950f510be04beff
BerkahEsport <berkahesport@gmail.com>
2023-05-09 10:05:59 +0700

    Update

---
+++ plugins/owner/screenshot.js
@@ -0,0 +1,17 @@
+const { fileTypeFromBuffer } = require('file-type');
+const wweb = require('whatsapp-web.js');
+const { MessageMedia } = wweb
+
+let handler = async (m, { conn }) => {
+    await conn.pupPage.setViewport({ width: 720, height: 1600})
+    let media = await conn.pupPage.screenshot({fullPage: true})
+    m.reply(new MessageMedia((await fileTypeFromBuffer(media)).mime, media.toString("base64")))
+}
+
+handler.help = ['sswa']
+handler.tags = ['owner']
+handler.command = /^(screenshot|ss)wa$/i
+
+handler.owner = true
+
+module.exports = handler;
