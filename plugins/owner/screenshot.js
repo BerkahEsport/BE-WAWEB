ed4ee7acc5eee8e4436e8c0bfa0a44822bfac439
BerkahEsport <berkahesport@gmail.com>
2023-05-09 18:46:23 +0700

    Update

--- plugins/owner/screenshot.js
+++ plugins/owner/screenshot.js
@@ -1,11 +1,11 @@
-const { fileTypeFromBuffer } = require('file-type');
+const { fromBuffer } = require('file-type');
 const wweb = require('whatsapp-web.js');
 const { MessageMedia } = wweb
 
 let handler = async (m, { conn }) => {
     await conn.pupPage.setViewport({ width: 720, height: 1600})
     let media = await conn.pupPage.screenshot({fullPage: true})
-    m.reply(new MessageMedia((await fileTypeFromBuffer(media)).mime, media.toString("base64")))
+    m.reply(new MessageMedia((await fromBuffer(media)).mime, media.toString("base64")))
 }
 
 handler.help = ['sswa']
